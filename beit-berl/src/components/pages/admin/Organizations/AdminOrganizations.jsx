a52529f274b280fb492def04a0e3e59b3bb72250
tehila-raviv <tehilaraviv@gmail.com>
2025-06-05 16:21:03 +0300

    update orgnizations page

--- beit-berl/src/components/pages/admin/Organizations/AdminOrganizations.jsx
+++ beit-berl/src/components/pages/admin/Organizations/AdminOrganizations.jsx
@@ -1,12 +1,11 @@
-// src/components/pages/admin/Organizations/AdminOrganizations.jsx
-
-import React from 'react';
+import OrganizationsList from '../../../Organizations/OrganizationsComp/OrganizationsList';
 
 export default function AdminOrganizations() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Admin Organizations</h1>
       <p>This is where admin organizations will be displayed.</p>
+      <OrganizationsList />
     </div>
   );
 }
