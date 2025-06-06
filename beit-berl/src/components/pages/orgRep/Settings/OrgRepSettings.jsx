14b2f51d8d0eeac155168182dcc674907fd7c3c1
tehila-raviv <tehilaraviv@gmail.com>
2025-06-06 15:59:56 +0300

    update orgRep pages

--- beit-berl/src/components/pages/orgRep/Settings/OrgRepSettings.jsx
+++ beit-berl/src/components/pages/orgRep/Settings/OrgRepSettings.jsx
@@ -1,12 +1,9 @@
 // src/components/pages/orgrep/settings/OrgRepSettings.jsx
-
-import React from 'react';
+import Settings from "../../../Settings/Settings";
 
 export default function OrgRepSettings() {
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>orgRep settings</h1>
-      <p>Welcome to the orgRep settings.</p>
       <Settings/>
     </div>
   );
