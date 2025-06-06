6ff21cf645ca0fecabf3bb4765cb68e4d6da367f
tehila-raviv <tehilaraviv@gmail.com>
2025-06-06 10:12:01 +0300

    add feedback functionality to both vc and orgRep

--- beit-berl/src/components/pages/vc/Organization/VcOrganization.jsx
+++ beit-berl/src/components/pages/vc/Organization/VcOrganization.jsx
@@ -1,12 +1,12 @@
 // src/components/pages/vc/organization/VcOrganization.jsx
-
-import React from 'react';
+import LimtedUsersData from '../../../UsersData/LimitedUsersData';
 
 export default function VcOrganization() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>vc organization</h1>
       <p>Welcome to the vc organization.</p>
+      <LimtedUsersData/>
     </div>
   );
 }
