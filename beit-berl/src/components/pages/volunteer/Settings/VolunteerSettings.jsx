e256bb336be4e09369d07f1e6a7e0a52c872591a
Saar101 <nrsaar@gmail.com>
2025-06-06 14:57:39 +0300

    Remove header and welcome message from VolunteerSettings component

--- beit-berl/src/components/pages/volunteer/Settings/VolunteerSettings.jsx
+++ beit-berl/src/components/pages/volunteer/Settings/VolunteerSettings.jsx
@@ -6,8 +6,6 @@ import Settings from '../../../Settings/Settings';
 export default function VolunteerSettings() {
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>volunteer settings</h1>
-      <p>Welcome to the volunteer settings.</p>
       <Settings/>
     </div>
   );
