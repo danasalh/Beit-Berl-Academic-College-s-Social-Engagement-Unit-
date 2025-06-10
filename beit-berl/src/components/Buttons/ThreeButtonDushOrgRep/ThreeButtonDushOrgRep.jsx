734d75bb9b83e02979aceec92f9fd9ba4e86f008
tehila-raviv <tehilaraviv@gmail.com>
2025-06-10 15:04:44 +0300

    update the navigation functionality and add styles

--- beit-berl/src/components/Buttons/ThreeButtonDushOrgRep/ThreeButtonDushOrgRep.jsx
+++ beit-berl/src/components/Buttons/ThreeButtonDushOrgRep/ThreeButtonDushOrgRep.jsx
@@ -1,17 +1,33 @@
-import React from "react";
-import { HiOutlineUserGroup, HiOutlineUser } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+import { HiLocationMarker , HiOutlineUserGroup, HiOutlineUser } from "react-icons/hi";
 import "./ThreeButtonDushOrgRep.css";
 
-const ThreeButtonDushOrgRep = () => {
+const ThreeButtonDushOrgRep = ({ onSectionsClick }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/orgRep/organization");
+  };
+
+  const handleSectionsClick = () => {
+    if (onSectionsClick) {
+      onSectionsClick();
+    }
+  };
+
   return (
     <div className="three-button-dush">
-      <button className="dush-button">
-        <HiOutlineUser className="dush-icon" />
-        <span>מתנדבים x</span>
+      <button className="dush-button" onClick={handleSectionsClick}>
+        <HiLocationMarker className="dush-icon" />
+        <span>סניפים x</span>
       </button>
-      <button className="dush-button">
+      <button className="dush-button" onClick={handleClick}>
         <HiOutlineUserGroup className="dush-icon" />
         <span>רכזים y</span>
+      </button>
+      <button className="dush-button" onClick={handleClick}>
+        <HiOutlineUser className="dush-icon"/>
+        <span>מתנדבים x</span>
       </button>
     </div>
   );
