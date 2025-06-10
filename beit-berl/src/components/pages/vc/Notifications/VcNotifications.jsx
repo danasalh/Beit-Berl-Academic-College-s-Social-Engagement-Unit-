c4bbedf675bdb5bc7c3d7098cd6985efec5e6fc4
tehila-raviv <tehilaraviv@gmail.com>
2025-06-10 15:06:15 +0300

    update vc notifications page

--- beit-berl/src/components/pages/vc/Notifications/VcNotifications.jsx
+++ beit-berl/src/components/pages/vc/Notifications/VcNotifications.jsx
@@ -4,8 +4,7 @@ import NotificauonsPanel from '../../../Notifications/NotificationsPanel/Notific
 export default function VcNotifications() {
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>vc notifications</h1>
-      <p>Welcome to the vc notifications.</p>
+      <h1 style ={{fontSize:'2em'}}>התראות</h1>
       <NotificauonsPanel />
     </div>
   );
