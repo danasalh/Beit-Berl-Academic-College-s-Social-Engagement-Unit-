f8cf49e3b7d153b8627e9123cf929755f416cb72
tehila-raviv <tehilaraviv@gmail.com>
2025-06-13 14:29:20 +0300

    update orgs files

--- beit-berl/src/components/Organizations/OrganizationsComp/OrganizationsList.jsx
+++ beit-berl/src/components/Organizations/OrganizationsComp/OrganizationsList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import OrgCard from './OrgCard';
 import OrgDetailsModal from './OrgDetailsModal';
 import { HiOutlineSearch } from "react-icons/hi";
