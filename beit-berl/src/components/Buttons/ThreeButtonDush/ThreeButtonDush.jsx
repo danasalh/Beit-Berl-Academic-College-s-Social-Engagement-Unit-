9d10d4f0e61e80a5884e40ea1af517e4c76cf474
tehila-raviv <tehilaraviv@gmail.com>
2025-06-22 17:56:18 +0300

    update 3-btns-dush-v

--- beit-berl/src/components/Buttons/ThreeButtonDush/ThreeButtonDush.jsx
+++ beit-berl/src/components/Buttons/ThreeButtonDush/ThreeButtonDush.jsx
@@ -10,16 +10,16 @@ const ThreeButtonDush = ({ onMarkHoursClick }) => {
   };
 
   return (
-    <div className="three-button-dush">
-      <button className="dush-button" onClick={onMarkHoursClick}>
-        <HiCheck className="dush-icon" />
+    <div className="three-button-dush-v">
+      <button className="dush-button-v" onClick={onMarkHoursClick}>
+        <HiCheck className="dush-icon-v" />
         <span>הזנת שעות התנדבות</span>
       </button>
-      <button className="dush-button" onClick={handleOrgSearchClick}>
-        <HiLibrary className="dush-icon" />
+      <button className="dush-button-v" onClick={handleOrgSearchClick}>
+        <HiLibrary className="dush-icon-v" />
         <span>חיפוש ארגון להתנדבות</span>
       </button>
-    <div className="dush-info">
+    <div className="dush-info-v">
         <h6>על מנת לקבל זכאות בגין ההתנדבות </h6>
         <p> <HiCheckCircle/> יש להקפיד להזין שעות התנדבות עם תום ההתנדבות.</p>
         <p> <HiCheckCircle/> תאריך הזנת השעות נשמר אוטומטית כמועד ביצוע ההתנדבות.</p>
