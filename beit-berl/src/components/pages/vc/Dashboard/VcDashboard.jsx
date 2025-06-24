f277996812eaf4de1224f5004b689d00be6b50fb
tehila-raviv <tehilaraviv@gmail.com>
2025-06-24 12:49:53 +0300

    fix vc dashboard styles

--- beit-berl/src/components/pages/vc/Dashboard/VcDashboard.jsx
+++ beit-berl/src/components/pages/vc/Dashboard/VcDashboard.jsx
@@ -1,7 +1,7 @@
 import './VcDashboard.css';
 import HoursList from '../../../HoursList/HoursList';
 import OrganizationsList from '../../../Organizations/OrganizationsList';
-import {HiCheckCircle} from "react-icons/hi";
+import { HiCheckCircle } from "react-icons/hi";
 
 export default function VcDashboard() {
   return (
@@ -10,19 +10,20 @@ export default function VcDashboard() {
         ברוך הבא
       </div>
       <div className='vc-dashboard-content'>
+        <div className="vc-hours-list">
+          <HoursList />
+        </div>
         <div className="vc-dashboard-header">
           <div className='org-info-card'>
             <h2>כרטיס הארגון שלך</h2>
-            <p> <HiCheckCircle/> דרך כרטיס הארגון שלך ניתן לצפות בפרטי הארגון</p>
-            <p> <HiCheckCircle/> יש להוסיף ולעדכן את פרטי יצירת הקשר כדי שמועמדים שיתעניינו בהתנדבות יוכלו ליצור עימך קשר </p>
+            <p> <HiCheckCircle /> דרך כרטיס הארגון שלך ניתן לצפות בפרטי הארגון</p>
+            <p> <HiCheckCircle /> יש להוסיף ולעדכן את פרטי יצירת הקשר כדי שמועמדים שיתעניינו בהתנדבות יוכלו ליצור עימך קשר </p>
           </div>
           <div className="org-preview">
             <OrganizationsList />
           </div>
         </div>
-        <div className="vc-hours-list">
-          <HoursList />
-        </div>
+
       </div>
     </div>
   );
